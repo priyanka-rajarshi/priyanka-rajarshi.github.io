11e6fb489a039200ea6d9eca6af16c7b5c80213d
priyanka-rajarshi <pchowdhury.bio@gmail.com>
2018-10-25 00:56:35 -0400

    second push

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 
+function main() {
+
 (function () {
    'use strict';
    
@@ -8,13 +10,37 @@
           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
           if (target.length) {
             $('html,body').animate({
-              scrollTop: target.offset().top
+              scrollTop: target.offset().top - 40
             }, 900);
             return false;
           }
         }
       });
 
+	
+    // Show Menu on Book
+    $(window).bind('scroll', function() {
+        var navHeight = $(window).height() - 500;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 80
+    });
+
+	// Hide nav on click
+  $(".navbar-nav li a").click(function (event) {
+    // check if window is small enough so dropdown is created
+    var toggle = $(".navbar-toggle").is(":visible");
+    if (toggle) {
+      $(".navbar-collapse").collapse('hide');
+    }
+  });
 	
   	// Portfolio isotope filter
     $(window).load(function() {
@@ -44,11 +70,36 @@
 
     });
 	
+  	// CounterUp
+	$(document).ready(function( $ ) {
+		if($("span.count").length > 0){	
+			$('span.count').counterUp({
+					delay: 10, // the delay time in ms
+			time: 1000 // the speed time in ms
+			});
+		}
+	});
+	
+	// Testimonial Slider
+	  	$(document).ready(function() {
+	      $("#testimonial").owlCarousel({
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+        });
+
+  	});
 	
     // Nivo Lightbox 
     $('.portfolio-item a').nivoLightbox({
             effect: 'slideDown',  
             keyboardNav: true,                            
         });
+ 
 
 }());
+
+
+}
+main();
